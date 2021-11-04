dc0ea7d5daf7a67b8f810b8f1e5291d71a0af01e
Gordon <gordon518168@yahoo.com>
2021-11-04 22:02:49 +0800

    use parameter of ?

--- api/getMerchants.js
+++ api/getMerchants.js
@@ -12,19 +12,22 @@ function searchMerchant(req, fn) {
 	let country=req.query.country;
 	let merchant=req.query.merchant;
 	let swhere="";
+	let param=[];
 	if(country && country.length>0) {
-		swhere=" where country='"+country+"'";
+		swhere=" where country=?";
+		param.push(country);
 	}
 	if(merchant && merchant.length>0) {
-		if(swhere.length>0)
+		if(swhere.length>0) {
 			swhere+=" and ";
-		else
+		} else {
 			swhere+=" where ";
-		swhere+=" name like '%"+merchant+"%'";
-
+		}		
+		swhere+="name like ?";
+		param.push("%"+merchant+"%");
 	}
 	let sql="select * from merchants"+swhere;
-	db.query(sql,[],function(ret){
+	db.query(sql,param,function(ret){
 		fn(ret);
 	});
 
